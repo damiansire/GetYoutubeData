43d61a3cab59dadaa211a193092e00879c9d64d4
Damian Sire <damiansire@hotmail.com>
2021-09-26 02:31:56 -0300

    exports method

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
+const { getAllComments, getPaginatedComments, getNextCommentsPage } = require("./controllers/commentsController")
 
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config()
+}
 
-(async () => {
+let apiKey = process.env.YOUTUBE_API_KEY;
+let videoId = "UIENaGNL6es";
 
-    const commentsController = require("./controllers/commentsController")
-
-    if (process.env.NODE_ENV != "production") {
-        require('dotenv').config()
-    }
-
-    let apiKey = process.env.YOUTUBE_API_KEY;
-    let videoId = "UIENaGNL6es";
-
-
-    const data3 = await commentsController.getAllComments(apiKey, videoId);
-    const data1 = await commentsController.getPaginatedComments(apiKey, videoId, 30);
-    const data2 = await commentsController.getNextCommentsPage(apiKey, videoId, data1.nextPageToken, 30);
-    console.log("xd")
-})()
-
+exports.getAllComments = getAllComments;
+exports.getPaginatedComments = getPaginatedComments;
+exports.getNextCommentsPage = getNextCommentsPage;
